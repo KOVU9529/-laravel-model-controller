34b44786b526314f99e59b625629cf5fcb1da543
KOVU9529 <bapgiordano@gmail.com>
2022-08-26 15:10:01 +0200

    css

--- laravel-model-controller/resources/views/home.blade.php
+++ laravel-model-controller/resources/views/home.blade.php
@@ -5,14 +5,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
 </head>
 <body>
     <ul>
         @foreach ($movies as $movie)
             <li>
-                {{$movie-> title}}
+                <span class="red">Titolo in italiano: {{$movie-> title}} </span> <br>
+                <span class="green">Titolo in lingua originale: {{$movie-> original_title}} </span> <br>
+                <span class="blu">Nazionalità: {{$movie-> nationality}}</span>  <br>
+                <span class="black">Data : {{$movie-> date}} </span><br>
+                <span class="violet">Voto : {{$movie-> vote}} </span> <br>
             </li>
         @endforeach
     </ul>
+    <div class="padrino">
+
+    </div>
 </body>
 </html>
